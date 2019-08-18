9880d97230cf6ec429c13d1860a084eb0a113302
meopkids@gmail.com <meopkids@gmail.com>
2019-08-18 09:56:17 +0800

    root sol1:newton method

---
+++ root/newton_mothod.py
@@ -0,0 +1,20 @@
+# x = y*y
+# write a function to evaluate root of x
+
+def root(x):
+    if x<0:
+        return False
+    else:
+        # Newton method
+        # guess = (x+x/y)/2
+        # good enough: (x/guess - guess) * 100 < 0.05
+        guess = 1
+        imporve = 1
+        while imporve >= 0.05:
+            guess = (guess+x/guess)/2
+            imporve = (x/guess - guess) * 100
+            if imporve<0:
+                imporve = - imporve
+        return guess
+
+root(2)
