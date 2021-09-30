729a9a2c9e1e1b2ad932e201c6a4cd7615c5921c
ArJhe <meopkids@gmail.com>
2021-09-30 15:23:55 +0800

    #0 cpp/8_StringToInteger.cpp

---
+++ cpp/8_StringToInteger.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int myAtoi(string s) {
+    
+
+    return 0;
+}
+
+int main(){
+    return 0;
+}
+// Use this template always on leetcode to improve runtime.
+static const int _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+   return 0;
+}();
+
