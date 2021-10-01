3231e6b5e8d72c71ffd8f2897d42a5db91796e11
ArJhe <meopkids@gmail.com>
2021-10-01 08:50:54 +0800

    #0 cpp/9_PalindromeNumber.cpp: add ref solution

--- cpp/9_PalindromeNumber.cpp
+++ cpp/9_PalindromeNumber.cpp
@@ -1,5 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
+// Runtime: 0 ms, faster than 100.00 % of C++ online submissions for Palindrome Number.
+// Memory Usage : 5.9 MB, less than 74.53 % of C++ online submissions for Palindrome Number.
+bool ref_isPalindrome(int x)
+{
+    if (x < 0) {
+        return false;
+    }
+    
+    int rev = 0;
+    int temp = x;
+    while (temp != 0) {
+        int digit = temp % 10;
+        temp /= 10;
+        if (rev > 2147483647 / 10)
+        {
+            return 0;
+        }
+        if (rev < -2147483647 / 10)
+        {
+            return 0;
+        }
+        rev = rev * 10 + digit;
+    }
+    if (rev == x)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+    
+}
+
 
 // Runtime: 8 ms, faster than 79.24% of C++ online submissions for Palindrome Number.
 // Memory Usage : 6.1 MB, less than 12.06 % of C++ online submissions for Palindrome Number.
@@ -10,7 +44,6 @@ bool my_isPalindrome(int x)
     string s = to_string(x);
     int len = s.length();
     int l =  % 2 == 0 ? len/2 - 1 : len-1/2 -1
-    int i = 0;
     int j = 0;
     while (j <= l)
     {
@@ -18,6 +51,7 @@ bool my_isPalindrome(int x)
             return false;
         j++;
     }
+
     return true;             
 }
 
---
+++ python/10_RegularExpression.py
@@ -0,0 +1,7 @@
+def isMatch( s: str, p: str) -> bool:
+    return false
+
+if __name__ == "__main__":
+    s = ""
+    p = "ca*b."
+    isMatch()
